f7255662c1e00fd083b94c7bcf03197f4c9194f9
Lars Djursner Rasmussen <ladr@itu.dk>
2020-08-25 21:00:06 +0200

    Test Driven Development of IsPrime method

--- core/getting-started/unit-testing-using-dotnet-test/PrimeService/PrimeService.cs
+++ core/getting-started/unit-testing-using-dotnet-test/PrimeService/PrimeService.cs
@@ -1,23 +1,14 @@
 ﻿using System;
 
-namespace Prime.Services
-{
-    public class PrimeService
-    {
-        public bool IsPrime(int candidate)
-        {
-            if (candidate < 2)
-            {
-                return false;
-            }
+namespace Prime.Services {
+    public class PrimeService {
+        public bool IsPrime(int candidate) {
+            if(candidate < 2) return false;
 
-            for (var divisor = 2; divisor <= Math.Sqrt(candidate); divisor++)
-            {
-                if (candidate % divisor == 0)
-                {
-                    return false;
-                }
+            for(var divisor = 2; divisor <= candidate; divisor++) {
+                if(candidate % divisor == 0) return false;
             }
+            
             return true;
         }
     }
